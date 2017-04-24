b495bea0f562d71b8b22663336cb08c3f6e9858a
Chang-Eop Kim <eopchang@gmail.com>
2017-04-24 17:41:01 +0900

    윈도우 버전 에러 수정.
    node attribute 윈도우에서 에러나는 부분 수정

---
+++ toCytoscape.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#networkx로 생성된 그래프 객체를 cytoscape에 import할 수 있는 엑셀파일로 저장해주는 모듈
+"""
+Created on Sun Mar 26 20:28:22 2017
+
+@author: Chang-Eop
+"""
+
+import os
+os.chdir('/Users/Chang-Eop/Desktop/GitHub/NetAnal')
+
+
+import pandas as pd
+import networkx as nx
+
+def interactions(G, title):
+    G.edges()
+    G_dic_frame = pd.DataFrame(G.edges(), columns = ['node 1', 'node 2']) #단순 interaction 쌍 without attribute
+    G_dic_frame.index.name = 'index' #cytoscape import시 에러막기 위해 이름 필요
+    #연결 정보 저장
+    G_dic_frame.to_excel(title + '.xlsx') 
+    
+   
+
+def node_attr(G, title):
+    #dictionary 구조에서 key순서 바뀌므로 반드시 sorted해야 함. 
+    G_dic_frame_node = pd.DataFrame(sorted(G.nodes()), columns = ['node name'])
+    G_dic_frame_node.index.name = 'index' #cytoscape import시 에러막기 위해 이름 필요
+    attrs_node = list(list(G.node.values())[0])
+    for i in attrs_node:
+        node_attr = list(sorted(nx.get_node_attributes(G,i).values()))
+        G_dic_frame_node[i+' (attr)'] = node_attr
+        
+    #node attribute 저장                 
+    G_dic_frame_node.to_excel(title + '.xlsx')
